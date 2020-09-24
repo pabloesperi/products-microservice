2a0394340a88642d16e404e652e4d4c49e6d4d5c
pabloesperi <pabloesperi@hotmail.com>
2020-09-24 13:26:03 +0200

    first commit

---
+++ src/test/java/com/proyects/microservices/app/productsmicroservice/ProductsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyects.microservices.app.productsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
